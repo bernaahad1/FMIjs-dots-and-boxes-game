012ce963e3b2af45b8c6c163e79f69207a20c34f
Teodora Slavova <morska_kotka1@abv.bg>
2022-12-03 12:09:09 +0200

    setup basic socket events

--- server.js
+++ server.js
@@ -14,6 +14,52 @@ server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 
 // Handle socket connection
+let users = []; //{username, ownedGames = [], id}
+const rooms = new Map(); //{owner, name, gridSize, playerNum}
+
 io.on('connection', socket =>{
     console.log(`New WS Connection. Connection id: ${socket.id}`)
+    socket.on("join server", (username) => {
+        const user = {
+            username,
+            id: socket.id,
+        }
+        users.push(user);
+        io.emit("new user", users);
+    })
+
+    socket.on("disconect", () =>{
+        //this is when the user leave
+        //it should remove him from users
+        //and to emit "delete room"
+    })
+
+    socket.on("join room", (roomName, cb) =>{
+        socket.join(roomName);
+        cb(rooms[roomName]);//?
+        socket.emit("connected",rooms[roomName]);//?
+    })
+
+    socket.on("leave room", () =>{
+        //you can leave the room
+    })
+
+    socket.on("create room",(roomName, gridSize, playerNum) =>{
+        const room = {
+            owner: socket.id,
+            name: roomName,
+            size: gridSize,
+            players: []
+        };
+
+        for(let i = 0; i < playerNum; i++){
+            room.players.push(null);
+        }
+        rooms.set(roomName, room);
+    })
+
+    socket.on("delete room", () =>{
+        //you can delete it only if you are the user
+        //or it should delete them if the user disconects
+    })
 })
