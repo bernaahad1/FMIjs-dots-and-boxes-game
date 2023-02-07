fea528118dc5c41d98cbed12902365a8e2732ce4
Berna Ahad <bernaahad1@gmail.com>
2023-02-07 19:51:20 +0200

    Fix webconfig + Tedi

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -9,10 +7,8 @@ const isProduction = process.env.NODE_ENV == "production";
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
-  // entry: "./src/index.js",
   entry: {
     client: "./src/index.js",
-    // server: "../server.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -29,9 +25,6 @@ const config = {
     }),
 
     new MiniCssExtractPlugin(),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -49,36 +42,16 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
 
-const serverConfig = {
-  entry: {
-    server: "../server.js",
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].bundle.js",
-  },
-  target: "node",
-  externals: {
-    express: "express",
-    bufferutil: "bufferutil",
-    "utf-8-validate": "utf-8-validate",
-  },
-};
 
 module.exports = () => {
   if (isProduction) {
     config.mode = "production";
-    serverConfig.mode = "production";
   } else {
     config.mode = "development";
-    serverConfig.mode = "development";
   }
-  return [config, serverConfig];
+  return [config];
 };
